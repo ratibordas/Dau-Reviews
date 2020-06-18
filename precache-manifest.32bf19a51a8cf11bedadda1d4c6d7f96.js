1f9accb5ecd930ffa06bb47c21481424f0cef409
owner <owner@owners-MBP.Dlink>
2020-06-18 19:16:34 +0300

    Updates

---
+++ precache-manifest.32bf19a51a8cf11bedadda1d4c6d7f96.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b10d9fd147d2d01d8cb22facac7ce5a",
+    "url": "/ratibordas/Dau-Reviews/index.html"
+  },
+  {
+    "revision": "35e94b0ef32a4e7a8b86",
+    "url": "/ratibordas/Dau-Reviews/static/css/3.fbc32063.chunk.css"
+  },
+  {
+    "revision": "fb374cce94ec8bc1fc1b",
+    "url": "/ratibordas/Dau-Reviews/static/css/main.7165bc07.chunk.css"
+  },
+  {
+    "revision": "e72a5722e8812d00ff59",
+    "url": "/ratibordas/Dau-Reviews/static/js/2.770d3948.chunk.js"
+  },
+  {
+    "revision": "4a8c02487a23b32abdf84044a925553a",
+    "url": "/ratibordas/Dau-Reviews/static/js/2.770d3948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35e94b0ef32a4e7a8b86",
+    "url": "/ratibordas/Dau-Reviews/static/js/3.9c7abb56.chunk.js"
+  },
+  {
+    "revision": "fb374cce94ec8bc1fc1b",
+    "url": "/ratibordas/Dau-Reviews/static/js/main.8a5dbd7f.chunk.js"
+  },
+  {
+    "revision": "c9f2a964d6417cae41f1",
+    "url": "/ratibordas/Dau-Reviews/static/js/runtime-main.7c34449a.js"
+  },
+  {
+    "revision": "83fe5e5456b57eafde049108a4de52ab",
+    "url": "/ratibordas/Dau-Reviews/static/media/1.83fe5e54.jpg"
+  },
+  {
+    "revision": "064050d22afb3fbd12c8df16d7d755a4",
+    "url": "/ratibordas/Dau-Reviews/static/media/10.064050d2.jpg"
+  },
+  {
+    "revision": "ad2542afc046ef3753292ebdd4f8f5a0",
+    "url": "/ratibordas/Dau-Reviews/static/media/11.ad2542af.jpg"
+  },
+  {
+    "revision": "ceb441162e16720f6fca3a77542987fa",
+    "url": "/ratibordas/Dau-Reviews/static/media/2.ceb44116.jpg"
+  },
+  {
+    "revision": "0ffd8c4733fb5051fe578ca9437c1cf7",
+    "url": "/ratibordas/Dau-Reviews/static/media/3.0ffd8c47.jpg"
+  },
+  {
+    "revision": "0f3ffd056a23643ed11afeba36bef01d",
+    "url": "/ratibordas/Dau-Reviews/static/media/4.0f3ffd05.jpg"
+  },
+  {
+    "revision": "3d41819e58864736f3ebaf15b725c477",
+    "url": "/ratibordas/Dau-Reviews/static/media/6.3d41819e.jpg"
+  },
+  {
+    "revision": "1e0e333d3d8b500f9ba94295cf8a1405",
+    "url": "/ratibordas/Dau-Reviews/static/media/7.1e0e333d.png"
+  },
+  {
+    "revision": "fcc3ffee4ac98fb562725b7b284d56fe",
+    "url": "/ratibordas/Dau-Reviews/static/media/8.fcc3ffee.jpg"
+  },
+  {
+    "revision": "bb150466abb953e93c9b60f6717dfb25",
+    "url": "/ratibordas/Dau-Reviews/static/media/DecimaMonoX.bb150466.woff"
+  },
+  {
+    "revision": "ebb5c0ec6594ae2dd48be3d0e6069cb0",
+    "url": "/ratibordas/Dau-Reviews/static/media/dau.ebb5c0ec.svg"
+  },
+  {
+    "revision": "c32c471a427a6ce5caf3ed3f95c893c5",
+    "url": "/ratibordas/Dau-Reviews/static/media/dau_natasha.c32c471a.md"
+  }
+]);
