6857da1ea050d631d3f7eb9bf75e364f838ec6a8
owner <owner@owners-MBP.Dlink>
2020-06-18 19:32:59 +0300

    Updates

---
+++ precache-manifest.c660c0ecee1436cacb8d6f646c8ac8f8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c181840d50861221d0dc3413a79e5a1",
+    "url": "/Dau-Reviews/index.html"
+  },
+  {
+    "revision": "78a0803160a509abaebf",
+    "url": "/Dau-Reviews/static/css/3.9700ff68.chunk.css"
+  },
+  {
+    "revision": "c3bed43d0ca7dc846e64",
+    "url": "/Dau-Reviews/static/css/main.688f0e5a.chunk.css"
+  },
+  {
+    "revision": "e72a5722e8812d00ff59",
+    "url": "/Dau-Reviews/static/js/2.770d3948.chunk.js"
+  },
+  {
+    "revision": "4a8c02487a23b32abdf84044a925553a",
+    "url": "/Dau-Reviews/static/js/2.770d3948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a0803160a509abaebf",
+    "url": "/Dau-Reviews/static/js/3.951380d0.chunk.js"
+  },
+  {
+    "revision": "c3bed43d0ca7dc846e64",
+    "url": "/Dau-Reviews/static/js/main.b4a488dc.chunk.js"
+  },
+  {
+    "revision": "3a965ecbd7b16c3f9524",
+    "url": "/Dau-Reviews/static/js/runtime-main.c83e1efa.js"
+  },
+  {
+    "revision": "83fe5e5456b57eafde049108a4de52ab",
+    "url": "/Dau-Reviews/static/media/1.83fe5e54.jpg"
+  },
+  {
+    "revision": "064050d22afb3fbd12c8df16d7d755a4",
+    "url": "/Dau-Reviews/static/media/10.064050d2.jpg"
+  },
+  {
+    "revision": "ad2542afc046ef3753292ebdd4f8f5a0",
+    "url": "/Dau-Reviews/static/media/11.ad2542af.jpg"
+  },
+  {
+    "revision": "ceb441162e16720f6fca3a77542987fa",
+    "url": "/Dau-Reviews/static/media/2.ceb44116.jpg"
+  },
+  {
+    "revision": "0ffd8c4733fb5051fe578ca9437c1cf7",
+    "url": "/Dau-Reviews/static/media/3.0ffd8c47.jpg"
+  },
+  {
+    "revision": "0f3ffd056a23643ed11afeba36bef01d",
+    "url": "/Dau-Reviews/static/media/4.0f3ffd05.jpg"
+  },
+  {
+    "revision": "3d41819e58864736f3ebaf15b725c477",
+    "url": "/Dau-Reviews/static/media/6.3d41819e.jpg"
+  },
+  {
+    "revision": "1e0e333d3d8b500f9ba94295cf8a1405",
+    "url": "/Dau-Reviews/static/media/7.1e0e333d.png"
+  },
+  {
+    "revision": "fcc3ffee4ac98fb562725b7b284d56fe",
+    "url": "/Dau-Reviews/static/media/8.fcc3ffee.jpg"
+  },
+  {
+    "revision": "bb150466abb953e93c9b60f6717dfb25",
+    "url": "/Dau-Reviews/static/media/DecimaMonoX.bb150466.woff"
+  },
+  {
+    "revision": "ebb5c0ec6594ae2dd48be3d0e6069cb0",
+    "url": "/Dau-Reviews/static/media/dau.ebb5c0ec.svg"
+  },
+  {
+    "revision": "c32c471a427a6ce5caf3ed3f95c893c5",
+    "url": "/Dau-Reviews/static/media/dau_natasha.c32c471a.md"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ratibordas/Dau-Reviews/precache-manifest.915179940833a012f7623ee2f8a92500.js"
+  "/Dau-Reviews/precache-manifest.c660c0ecee1436cacb8d6f646c8ac8f8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ratibordas/Dau-Reviews/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Dau-Reviews/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
